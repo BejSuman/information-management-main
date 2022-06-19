6a1373795953ecca07d0ff21df935c945983d262
Subham <subham.ghorui@codesigma.tech>
2022-06-19 10:37:32 +0530

    check

--- app/Http/Controllers/BiometricController.php
+++ app/Http/Controllers/BiometricController.php
@@ -14,6 +14,10 @@ class BiometricController extends Controller
 
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index(Request $request)
     {
         return view('pages.biometric');
